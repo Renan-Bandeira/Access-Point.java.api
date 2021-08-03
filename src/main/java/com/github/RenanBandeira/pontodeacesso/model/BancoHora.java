44d4b4acb3964405cc589861eefc476ad8cc3976
renanbandeira94 <67717214+Renan-Bandeira@users.noreply.github.com>
2021-08-02 21:19:38 -0300

    Adição da chave composta na classe BancoHora

--- src/main/java/com/github/RenanBandeira/pontodeacesso/model/BancoHora.java
+++ src/main/java/com/github/RenanBandeira/pontodeacesso/model/BancoHora.java
@@ -2,7 +2,10 @@ package com.github.RenanBandeira.pontodeacesso.model;
 
 import lombok.*;
 
-import javax.persistence.ManyToOne;
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
+import java.io.Serializable;
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Getter
@@ -13,13 +16,21 @@ import java.time.LocalDateTime;
 @Builder
 
 public class BancoHora {
-    private Long id;
-    private String categoriaUsuario;
+
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @EqualsAndHashCode
+    @Embeddable
+    public class BancoHoraId implements Serializable {
+        private long idMovimentacao;
+        private long idUsuario;
+        private long idBancoHora;
+}
+    @EmbeddedId
+    private BancoHoraId id;
     private LocalDateTime dataTrabalhada;
-    private int horasTrabalhadas;
-    private int totalHorasTrabalhadas;
-    @ManyToOne
-    private Movimentacao movimentacao;
-    @ManyToOne
-    private Usuario usuario;
+    private BigDecimal horasTrabalhadas;
+    private BigDecimal saldoHoras;
+
 }
