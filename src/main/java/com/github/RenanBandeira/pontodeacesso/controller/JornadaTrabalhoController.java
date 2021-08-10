bf596ec9aa08be98974a2113a39f518bbf31b7c2
renanbandeira94 <67717214+Renan-Bandeira@users.noreply.github.com>
2021-08-09 22:55:14 -0300

    nova função em JornadaControle

--- src/main/java/com/github/RenanBandeira/pontodeacesso/controller/JornadaTrabalhoController.java
+++ src/main/java/com/github/RenanBandeira/pontodeacesso/controller/JornadaTrabalhoController.java
@@ -1,7 +1,10 @@
 package com.github.RenanBandeira.pontodeacesso.controller;
 
 import com.github.RenanBandeira.pontodeacesso.model.JornadaTrabalho;
+import com.github.RenanBandeira.pontodeacesso.service.JornadaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/jornada")
 public class JornadaTrabalhoController {
 
+    @Autowired
+    JornadaService jornadaService;
+
     @PostMapping
-    public JornadaTrabalho CreateJornada(){
-        return null;
+    public JornadaTrabalho CreateJornada(@RequestBody JornadaTrabalho jornadaTrabalho){
+        return jornadaService.saveJornada(jornadaTrabalho);
 
     }
 }
--- src/main/java/com/github/RenanBandeira/pontodeacesso/service/JornadaService.java
+++ src/main/java/com/github/RenanBandeira/pontodeacesso/service/JornadaService.java
@@ -1,5 +1,6 @@
 package com.github.RenanBandeira.pontodeacesso.service;
 
+import com.github.RenanBandeira.pontodeacesso.model.JornadaTrabalho;
 import com.github.RenanBandeira.pontodeacesso.repository.JornadaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,4 +15,11 @@ public class JornadaService {
     public JornadaService(JornadaRepository jornadaRepository) {
         this.jornadaRepository = jornadaRepository;
     }
+
+
+    public JornadaTrabalho saveJornada(JornadaTrabalho jornadaTrabalho){
+       return jornadaRepository.save(jornadaTrabalho);
+
+    }
+
 }
